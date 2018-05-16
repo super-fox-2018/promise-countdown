9f4012bf8b8bbf09f8d648d0d2c8b85e3b107732
alprak <you@example.com>
2018-05-16 16:01:10 +0700

    done

--- views.js
+++ views.js
@@ -6,6 +6,25 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  var num =seconds
+  if(num>60 && num%60<10) {
+			if(num<600) {
+				seconds = `0${Math.floor(num/60)} : 0${num%60}`	
+			}else{
+				seconds = `${Math.floor(num/60)} : 0${num%60}`
+			}
+		}else if(num>60 && num%60>=10){
+			if(num<600) {
+				seconds = `0${Math.floor(num/60)} : ${num%60}`	
+			}else{
+				seconds = `${Math.floor(num/60)} : ${num%60}`
+			}
+
+		}else{
+			seconds = `00:${num} `
+		}
+
+  console.log(chalk.cyan(figlet.textSync(seconds)))
 };
 
 module.exports = { displayCountdown };
