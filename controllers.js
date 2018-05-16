581e01fac2c498a7957f54e7cdaae3c709194dc0
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-05-16 11:57:47 +0700

    countdown promise finished

--- controllers.js
+++ controllers.js
@@ -6,10 +6,10 @@ const timer = seconds =>
     const then = now + seconds * 1000;
 
     displayCountdown(seconds - 1);
-    setInterval(() => {
+    var Interval = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
       if (secondsLeft < 0) {
-        clearInterval();
+        clearInterval(Interval);
         resolve();
       } else {
         displayCountdown(secondsLeft);
