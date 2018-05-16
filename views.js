02dac51a569cf3c18a407b911130865a286bacd4
mario <panggihmario@gmail.com>
2018-05-16 17:03:52 +0700

    countdown

--- views.js
+++ views.js
@@ -6,6 +6,16 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let menit = Math.floor(seconds / 60)
+  let second = seconds - (menit * 60)
+  if (String(second).length === 1) {
+    second = '0' + second
+  }
+  if (String(menit).length === 1) {
+    menit = '0' + menit
+  }
+
+  console.log(chalk.white.bold(figlet.textSync(menit + " : " + second)))
 };
 
 module.exports = { displayCountdown };
