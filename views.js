ac177324c55582bbb53068bfce07aa1e53b57696
shantidyah <shanti.dyah@gmail.com>
2018-05-16 15:52:56 +0700

    promise countdown

--- views.js
+++ views.js
@@ -4,8 +4,18 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
+  var time = secondsToMinutes(seconds)
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  console.log(chalk.cyan(figlet.textSync(time)));
 };
 
+function secondsToMinutes(seconds) {
+  var jam = Math.floor(seconds/60);
+  var detik = seconds%60;
+  var stringJam = (jam<10)? '0'+jam.toString() : jam.toString();
+  var stringDetik = (detik<10)? '0'+detik.toString() : detik.toString();
+  var clock = stringJam +':'+ stringDetik;
+  return clock;
+}
 module.exports = { displayCountdown };
