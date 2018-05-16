a66a920f6742ca468526e1fa0b661aa785b5013c
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-05-16 19:18:20 +0700

    brama-prasetyo

--- controllers.js
+++ controllers.js
@@ -4,12 +4,13 @@ const timer = seconds =>
   new Promise((resolve, reject) => {
     const now = Date.now();
     const then = now + seconds * 1000;
-
+    console.log(now)
+    console.log(then)
     displayCountdown(seconds - 1);
-    setInterval(() => {
+    let id = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
       if (secondsLeft < 0) {
-        clearInterval();
+        clearInterval(id);
         resolve();
       } else {
         displayCountdown(secondsLeft);
